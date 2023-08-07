350017845098002a990066fe3d1c12a3c7c0330e
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-07 10:29:39 +0100

    add styling to header element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
     <div>
-    <h1>FreedomPraise Pomodoro App</h1>
+    <h3>FreedomPraise's Pomodoro App</h3>
     </div>
     );
 };
