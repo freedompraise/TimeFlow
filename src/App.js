8a6a9a03750001d768cb1257700697ea8a8c8df8
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-08 02:39:56 +0100

    create session and break elements

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ function App() {
     <div className="App">
        <Header className="App-header" />
        <Footer className="App-footer" />
+       <p className="break-label" id='break-label'  >Break Length</p>
+       <p className="break-length" id='break-length'>5</p>
+       <p className="session-label" id='session-label'>Session Length</p>
+       <p className="session-length" id='session-length'>25</p>
+       <button className="break-decrement" id='break-decrement' >-</button>
+        <button className="break-increment" id='break-increment' >+</button>
+        <button className="session-decrement" id='session-decrement' >-</button>  
+        <button className="session-increment" id='session-increment' >+</button>     
+        <p className="timer-label" id='timer-label'>Session</p>
+        <p className="time-left" id='time-left'>25:00</p> 
+        <button className="start_stop" id='start_stop' >Start/Stop</button>
+        <button className="reset" id='reset' >Reset</button>
+
 
     </div>
   );
