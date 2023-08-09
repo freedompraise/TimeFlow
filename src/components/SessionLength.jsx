01b295c4a2c62dbfe9f4f37a4efc8c5d8b7c49e3
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-10 00:01:56 +0100

    create increment/decrement session methods

--- src/components/SessionLength.jsx
+++ src/components/SessionLength.jsx
@@ -2,18 +2,32 @@ import React from 'react';
 
 class Session extends React.Component {
         constructor(props) {
-        super(props);
-        this.state = {
-        sessionLength: 25
+           super(props);
+           this.state = {
+              sessionLength: 25
+            }
+            this.incrementSession = this.incrementSession.bind(this);
+            this.decrementSession = this.decrementSession.bind(this);
         }
+
+        incrementSession() {
+                this.setState({
+                        sessionLength: this.state.sessionLength + 1
+                })
         }
+        decrementSession() {
+                this.setState({
+                        sessionLength: this.state.sessionLength - 1
+                })
+        }
+
         render () {
         return (
         <div>
         <p className="session-label" id='session-label'>Session Length</p>
         <p className="session-length" id='session-length'>{this.state.sessionLength}</p>
-        <button className="session-decrement" id='session-decrement' >-</button>  
-        <button className="session-increment" id='session-increment' >+</button> 
+        <button className="session-decrement" id='session-decrement' onClick={this.decrementSession} >-</button>  
+        <button className="session-increment" id='session-increment' onClick={this.incrementSession}>+</button> 
        </div>
         );
         }
