ea95b60e854c6968e0a295c906d590f6749ed484
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-09 23:31:22 +0100

    make sessionlength component stateful

--- src/components/SessionLength.jsx
+++ src/components/SessionLength.jsx
@@ -1,13 +1,22 @@
-const Session = () => {
-        const sessionLength = 25;
+import React from 'react';
+
+class Session extends React.Component {
+        constructor(props) {
+        super(props);
+        this.state = {
+        sessionLength: 25
+        }
+        }
+        render () {
         return (
         <div>
         <p className="session-label" id='session-label'>Session Length</p>
-        <p className="session-length" id='session-length'>{sessionLength}</p>
+        <p className="session-length" id='session-length'>{this.state.sessionLength}</p>
         <button className="session-decrement" id='session-decrement' >-</button>  
         <button className="session-increment" id='session-increment' >+</button> 
        </div>
         );
+        }
 };
 
 export default Session;
