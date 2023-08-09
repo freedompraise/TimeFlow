065c95a58ab8bf2488e2407cbe59a14a878e4d71
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-09 23:39:28 +0100

    make breaklength component stateful

--- src/components/BreakLength.jsx
+++ src/components/BreakLength.jsx
@@ -1,12 +1,22 @@
-const Break = () => {
-    return (
+import React from 'react';
+
+class Break extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            breakLength: 5
+        }
+    }
+        render () {
+        return (
         <div>
         <p className="break-label" id='break-label'  >Break Length</p>
-        <p className="break-length" id='break-length'>5</p>
+        <p className="break-length" id='break-length'>{this.state.breakLength}</p>
         <button className="break-decrement" id='break-decrement' >-</button>
         <button className="break-increment" id='break-increment' >+</button>
         </div>
-    )
+    );
 }
+};
 
 export default Break;
