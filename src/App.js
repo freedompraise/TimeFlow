681465f29d1cc183e0865b734b4e94b4893428e3
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-10 11:42:41 +0100

    update app component to extend react component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
@@ -5,17 +6,25 @@ import Session from './components/SessionLength';
 import Break from './components/BreakLength';
 import Timer from './components/Timer';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+      setSessionLength : (newLength) => this.setState({ sessionLength: newLength })
+    }
+  }
+  render() {
+      return (
     <div className="App">
        <Header className="App-header" />
        <Footer className="App-footer" />
-       <Break />    
-       <Session />
-       <Timer />
-
+       <Break breakLength={this.state.breakLength} />    
+       <Session sessionLength={this.state.sessionLength} setSessionLength={newLength => this.setState({ sessionLength: newLength })} />
+       <Timer sessionLength={this.state.sessionLength} />
     </div>
   );
 }
-
+};
 export default App;
