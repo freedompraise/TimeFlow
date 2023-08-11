0b71bb49b86b3163eced76b37c2b29ef0410e7b6
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-11 02:05:16 +0100

    delete state from Break constructor

--- src/components/BreakLength.jsx
+++ src/components/BreakLength.jsx
@@ -1,35 +1,48 @@
-import React from 'react';
+import React from "react";
 
 class Break extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            breakLength: 5
-        }
-        this.incrementBreak = this.incrementBreak.bind(this);
-        this.decrementBreak = this.decrementBreak.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.incrementBreak = this.incrementBreak.bind(this);
+    this.decrementBreak = this.decrementBreak.bind(this);
+  }
 
-    incrementBreak() {
-        this.setState({
-            breakLength: this.state.breakLength + 1
-        })
-    }
-    decrementBreak() {
-        this.setState({
-            breakLength: this.state.breakLength - 1
-        })
-    }
-        render () {
-        return (
-        <div>
-        <p className="break-label" id='break-label'  >Break Length</p>
-        <p className="break-length" id='break-length'>{this.state.breakLength}</p>
-        <button className="break-decrement" id='break-decrement' onClick={this.decrementBreak}>-</button>
-        <button className="break-increment" id='break-increment' onClick={this.incrementBreak}>+</button>
-        </div>
+  incrementBreak() {
+    this.setState({
+      breakLength: this.state.breakLength + 1,
+    });
+  }
+  decrementBreak() {
+    this.setState({
+      breakLength: this.state.breakLength - 1,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p className="break-label" id="break-label">
+          Break Length
+        </p>
+        <p className="break-length" id="break-length">
+          {this.state.breakLength}
+        </p>
+        <button
+          className="break-decrement"
+          id="break-decrement"
+          onClick={this.decrementBreak}
+        >
+          -
+        </button>
+        <button
+          className="break-increment"
+          id="break-increment"
+          onClick={this.incrementBreak}
+        >
+          +
+        </button>
+      </div>
     );
+  }
 }
-};
 
 export default Break;
