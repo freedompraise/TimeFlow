4706a56ae6c8032ed8dba071aee0067178acdcf7
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-08-11 02:33:18 +0100

    Update-break-component (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Session from './components/SessionLength';
-import Break from './components/BreakLength';
-import Timer from './components/Timer';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Session from "./components/SessionLength";
+import Break from "./components/BreakLength";
+import Timer from "./components/Timer";
 
 class App extends Component {
   constructor(props) {
@@ -12,35 +12,50 @@ class App extends Component {
     this.state = {
       breakLength: 5,
       sessionLength: 25,
-      isTimerRunning : false,
-      setIsTimerRunning : (newIsTimerRunning) => this.setState(
-        { 
-          isTimerRunning: newIsTimerRunning 
-        }), 
-      setSessionLength : (newLength) => this.setState(
-        { 
-          sessionLength: newLength 
-        })
-      }
+      isTimerRunning: false,
+
+      setIsTimerRunning: (newIsTimerRunning) =>
+        this.setState({
+          isTimerRunning: newIsTimerRunning,
+        }),
+      setSessionLength: (newLength) =>
+        this.setState({
+          sessionLength: newLength,
+        }),
+      setBreakLength: (newLength) =>
+        this.setState({
+          breakLength: newLength,
+        }),
+    };
   }
   render() {
-      return (
-        <div className="App">
-          <Header className="App-header" />
-          <Break breakLength={this.state.breakLength} />    
-          <Session 
-              sessionLength={this.state.sessionLength} 
-              setSessionLength={newLength => this.setState({ sessionLength: newLength })} 
-              isTimerRunning={this.state.isTimerRunning}
-              />
-          <Timer 
-              sessionLength={this.state.sessionLength} 
-              isTimerRunning={this.state.isTimerRunning}
-              setIsTimerRunning={newIsTimerRunning => this.setState({ isTimerRunning: newIsTimerRunning })}
-              />
-          <Footer className="App-footer" />
-        </div>
-  );
+    return (
+      <div className="App">
+        <Header className="App-header" />
+        <Break
+          breakLength={this.state.breakLength}
+          sessionLength={this.state.sessionLength}
+          setBreakLength={(newLength) =>
+            this.setState({ breakLength: newLength })
+          }
+        />
+        <Session
+          sessionLength={this.state.sessionLength}
+          setSessionLength={(newLength) =>
+            this.setState({ sessionLength: newLength })
+          }
+          isTimerRunning={this.state.isTimerRunning}
+        />
+        <Timer
+          sessionLength={this.state.sessionLength}
+          isTimerRunning={this.state.isTimerRunning}
+          setIsTimerRunning={(newIsTimerRunning) =>
+            this.setState({ isTimerRunning: newIsTimerRunning })
+          }
+        />
+        <Footer className="App-footer" />
+      </div>
+    );
+  }
 }
-};
 export default App;
--- src/components/BreakLength.jsx
+++ src/components/BreakLength.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
+import React from "react";
 
 class Break extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            breakLength: 5
-        }
-        this.incrementBreak = this.incrementBreak.bind(this);
-        this.decrementBreak = this.decrementBreak.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.incrementBreak = this.incrementBreak.bind(this);
+    this.decrementBreak = this.decrementBreak.bind(this);
+  }
 
-    incrementBreak() {
-        this.setState({
-            breakLength: this.state.breakLength + 1
-        })
+  incrementBreak() {
+    if (
+      this.props.breakLength < 10 &&
+      this.props.breakLength < this.props.sessionLength
+    ) {
+      // add condition to prevent breakLength from going above 10 minutes or sessionLength
+      this.props.setBreakLength(this.props.breakLength + 1);
     }
-    decrementBreak() {
-        this.setState({
-            breakLength: this.state.breakLength - 1
-        })
+  }
+  decrementBreak() {
+    if (this.props.breakLength > 1) {
+      // add condition to prevent breakLength from going below 1 minute
+      this.props.setBreakLength(this.props.breakLength - 1);
     }
-        render () {
-        return (
-        <div>
-        <p className="break-label" id='break-label'  >Break Length</p>
-        <p className="break-length" id='break-length'>{this.state.breakLength}</p>
-        <button className="break-decrement" id='break-decrement' onClick={this.decrementBreak}>-</button>
-        <button className="break-increment" id='break-increment' onClick={this.incrementBreak}>+</button>
-        </div>
+  }
+
+  render() {
+    return (
+      <div>
+        <p className="break-label" id="break-label">
+          Break Length
+        </p>
+        <p className="break-length" id="break-length">
+          {this.props.breakLength}
+        </p>
+        <button
+          className="break-decrement"
+          id="break-decrement"
+          onClick={this.decrementBreak}
+        >
+          -
+        </button>
+        <button
+          className="break-increment"
+          id="break-increment"
+          onClick={this.incrementBreak}
+        >
+          +
+        </button>
+      </div>
     );
+  }
 }
-};
 
 export default Break;
