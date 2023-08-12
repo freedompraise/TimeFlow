a69c317fc2a1b3f97c71d2b0f2c10c0f15fe0a4d
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-12 13:18:28 +0100

    update app name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="text-white text-center py-2">
-      <h3 className="text-xl font-semibold">FreedomPraise's Pomodoro App</h3>
+      <h3 className="text-2xl font-bold tracking-wide">TimeFlow</h3>
     </div>
   );
 };
