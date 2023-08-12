3ac14c6c2952fbbeee1b6988bbef9191b4a80661
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-12 04:49:21 +0100

    add styling to Footer and Header components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
-const Footer = () => {
-    return (<h3>Proudly built by 
-        <a 
-        href="https://linkfree.io/freedompraise"
-        target = "_blank"
-        >
-             Praise Freedom Dike
-        </a>
-    </h3>)
-}
+import React from "react";
 
-export default Footer ;
+const Footer = () => {
+  return (
+    <div className="text-white text-center py-2">
+      <h3 className="text-sm">
+        Proudly built by{" "}
+        <a
+          href="https://linkfree.io/freedompraise"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-300 hover:text-blue-200"
+        >
+          Praise Freedom Dike
+        </a>
+      </h3>
+    </div>
+  );
+};
+
+export default Footer;
