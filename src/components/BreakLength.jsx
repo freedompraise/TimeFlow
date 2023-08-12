acbc55e6b21397d328219776be28d1f90e4f4591
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-12 04:11:25 +0100

    apply tailwind to break and session

--- src/components/BreakLength.jsx
+++ src/components/BreakLength.jsx
@@ -25,30 +25,25 @@ class Break extends React.Component {
 
   render() {
     return (
-      <div>
-        <p className="break-label" id="break-label">
-          Break Length
-        </p>
-        <p className="break-length" id="break-length">
-          {this.props.breakLength}
-        </p>
-        <button
-          className="break-decrement"
-          id="break-decrement"
-          onClick={this.decrementBreak}
-        >
-          -
-        </button>
-        <button
-          className="break-increment"
-          id="break-increment"
-          onClick={this.incrementBreak}
-        >
-          +
-        </button>
+      <div className="bg-gray-200 p-4 rounded-md shadow-md text-center">
+        <p className="text-lg font-semibold">Break Length</p>
+        <div className="flex justify-center items-center mt-2">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-l"
+            onClick={this.props.decrementBreak}
+          >
+            -
+          </button>
+          <p className="text-xl mx-4">{this.props.breakLength}</p>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-r"
+            onClick={this.props.incrementBreak}
+          >
+            +
+          </button>
+        </div>
       </div>
     );
   }
 }
-
 export default Break;
