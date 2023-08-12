7e640e7761f965cd4c67f750c2e613fdd0b4bd61
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-12 13:28:44 +0100

    make header elements bigger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="text-white text-center py-2">
-      <h3 className="text-2xl font-bold tracking-wide">TimeFlow</h3>
+    <div className="text-white text-center py-3">
+      <h3 className="text-4xl font-bold tracking-wide">TimeFlow</h3>
+      <p className="text-lg text-gray-300 mt-2">
+        Stay Focused, Work Efficiently
+      </p>
     </div>
   );
 };
