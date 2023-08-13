2b2d84fa6407a1dfe536764a87e259a3dc752bd3
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-13 20:45:07 +0100

    test if all components are rendered

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,27 @@ test("renders App component without errors", () => {
   const headerElement = screen.getByText(/TimeFlow/i);
   expect(headerElement).toBeInTheDocument();
 });
+
+test("renders the Session Component", () => {
+  render(<App />);
+  const sessionElement = screen.getByText(/Session Length/i);
+  expect(sessionElement).toBeInTheDocument();
+});
+
+test("renders the Break component", () => {
+  render(<App />);
+  const breakElement = screen.getByText(/Break Length/i);
+  expect(breakElement).toBeInTheDocument();
+});
+
+test("renders the Timer component", () => {
+  render(<App />);
+  const timerElement = screen.getByText(/Start/);
+  expect(timerElement).toBeInTheDocument();
+});
+
+test("renders the footer", () => {
+  render(<App />);
+  footerElement = screen.getByText(/built by/i);
+  expect(footerElement.toBeInTheDocument());
+});
