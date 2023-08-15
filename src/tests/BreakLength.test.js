ef798e9442d1d8f6b7650c2fe05316376d248dcc
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-15 21:05:28 +0100

    resolve break tests

--- src/tests/BreakLength.test.js
+++ src/tests/BreakLength.test.js
@@ -3,23 +3,49 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Break from "../components/BreakLength";
 
 test("clicking decrement button reduces break length", () => {
-  const setBreakLengthMock = jest.fn(); // Create a mock function
-
-  render(<Break breakLength={5} setBreakLength={setBreakLengthMock} />);
+  const setBreakLength = jest.fn();
+  render(
+    <Break breakLength={5} setBreakLength={setBreakLength} sessionLength={25} />
+  );
 
   const decrementButton = screen.getByTestId("break-decrement");
   fireEvent.click(decrementButton);
 
-  expect(setBreakLengthMock).toHaveBeenCalledWith(4); // Ensure the mock function was called with the correct value
+  expect(setBreakLength).toHaveBeenCalledWith(4); // Assuming the length decrements by 1
 });
 
 test("clicking increment button increases break length", () => {
-  const setBreakLengthMock = jest.fn(); // Create a mock function
-
-  render(<Break breakLength={5} setBreakLength={setBreakLengthMock} />);
+  const setBreakLength = jest.fn();
+  render(
+    <Break breakLength={5} setBreakLength={setBreakLength} sessionLength={25} />
+  );
 
   const incrementButton = screen.getByTestId("break-increment");
   fireEvent.click(incrementButton);
 
-  expect(setBreakLengthMock).toHaveBeenCalledWith(6); // Ensure the mock function was called with the correct value
+  expect(setBreakLength).toHaveBeenCalledWith(6); // Assuming the length increments by 1
+});
+
+test("break length does not go above 10 minutes or session length", () => {
+  const setBreakLength = jest.fn();
+  render(
+    <Break breakLength={9} setBreakLength={setBreakLength} sessionLength={25} />
+  );
+
+  const incrementButton = screen.getByTestId("break-increment");
+  fireEvent.click(incrementButton);
+
+  expect(setBreakLength).toHaveBeenCalledWith(10); // Break length capped at 10
+});
+
+test("break length does not go below 1 minute", () => {
+  const setBreakLength = jest.fn();
+  render(
+    <Break breakLength={2} setBreakLength={setBreakLength} sessionLength={25} />
+  );
+
+  const decrementButton = screen.getByTestId("break-decrement");
+  fireEvent.click(decrementButton);
+
+  expect(setBreakLength).toHaveBeenCalledWith(1); // Break length cannot go below 1
 });
