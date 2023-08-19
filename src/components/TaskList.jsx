b80a93861e4a2192191c79dac01b845a7de61d6a
Praise Freedom Dike <dikepraise119@gmail.com>
2023-08-19 15:22:31 +0100

    implement task list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 class TaskList extends Component {
   constructor(props) {
@@ -41,26 +43,46 @@ class TaskList extends Component {
 
   render() {
     return (
-      <div className="task-list">
-        <h2>Task List</h2>
-        <div className="task-list__input">
+      <div className="rounded-lg p-8 shadow-md space-y-4">
+        <h2 className="text-2xl font-semibold mb-4 text-center">Task List</h2>
+        <div className="task-list__input mb-2 flex items-center">
           <input
             type="text"
             placeholder="Enter a task"
             onChange={(e) => this.setState({ newTask: e.target.value })}
             value={this.state.newTask}
+            className="rounded-md py-2 px-2 w-40 text-black"
           />
-          <button onClick={this.addTask}>Add Task</button>
+          <button
+            onClick={this.addTask}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-2 rounded-md ml-2"
+          >
+            Add Task
+          </button>
         </div>
         <ul className="task-list__list">
           {this.state.tasks.map((task, index) => (
-            <li key={index}>
-              <span onClick={() => this.markTaskCompleted(index)}>{task}</span>
-              <button onClick={() => this.removeTask(index)}>Remove</button>/
+            <li key={index} className="mb-0 flex items-center">
+              <input
+                type="checkbox"
+                onChange={() => this.markTaskCompleted(index)}
+                checked={task.startsWith("✅")}
+                className="mr-2"
+              />
+              <span className="text-lg">{task.replace("✅", "")}</span>
+              <button
+                className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-2 rounded-md ml-auto"
+                onClick={() => this.removeTask(index)}
+              >
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
             </li>
           ))}
         </ul>
-        <button onClick={this.clearCompletedTasks}>
+        <button
+          onClick={this.clearCompletedTasks}
+          className="text-red-300 hover:bg-red-600 font-semibold py-4 px-4 rounded-md mx-auto block mt-4"
+        >
           Clear Completed Tasks
         </button>
       </div>
