e9fd52afe1a7a11d07ca8815b8131fa532623338
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-08-21 12:52:55 +0100

    Connect Task List Component to Pomodoro Functionality in the App (#23)
    * create util for handling the tasks
    
    * add state properties for tasklist
    
    * import functions from taskutil
    
    * update manageTasks function
    
    * delete hasCompletedTasks conditional
    
    * fix functions for deleting and clearing completed tasks
    
    * create toggleTaskCompleted
    
    * add toggle case in manageTasks()
    
    * remove background colour from input fields
    
    * update task placeholder
    
    * add label to the pomodoros input
    
    * give spacing betwee input boxes
    
    * reduce size of button for clearing completed tasks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,66 +1,79 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { removeTask, clearCompletedTasks } from "../utils/taskUtils";
 
 class TaskList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: [],
-      newTask: "",
+      taskInput: "",
+      pomodorosInput: 1, // default number of pomodoros
+      tasks: this.props.tasks || [], // tasks from props
     };
   }
   addTask = () => {
-    // add the new task to the tasks array
-    if (this.state.newTask.trim() !== "") {
-      this.setState({
-        tasks: [...this.state.tasks, this.state.newTask],
-        newTask: "",
-      });
+    const { taskInput, pomodorosInput } = this.state;
+    if (taskInput.trim() === "") {
+      return;
+    }
+    this.props.onUpdateTasks("add", null, taskInput, pomodorosInput);
+    this.setState({ taskInput: "", pomodorosInput: 1 });
+  };
+
+  toggleTaskCompleted = (index) => {
+    const updatedTasks = [...this.state.tasks];
+    if (updatedTasks[index]) {
+      updatedTasks[index].completed = !updatedTasks[index].completed;
+      this.setState({ tasks: updatedTasks });
     }
   };
-  markTaskCompleted(index) {
-    const updatedTasks = [...this.state.tasks];
-    if (updatedTasks[index].startsWith("✅")) {
-      // Task is already marked as completed, unmark it
-      updatedTasks[index] = updatedTasks[index].substring(1); // Remove the "✅"
-    } else {
-      // Task is not completed, mark it
-      updatedTasks[index] = "✅" + updatedTasks[index];
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.tasks !== this.props.tasks) {
+      this.setState({ tasks: this.props.tasks || [] });
     }
-    this.setState({ tasks: updatedTasks });
   }
 
   removeTask = (index) => {
-    // remove the task from the tasks array
-    const updatedTasks = [...this.state.tasks];
-    updatedTasks.splice(index, 1);
-    this.setState({ tasks: updatedTasks });
+    const updatedTasks = removeTask(this.props.tasks, index);
+    this.props.onUpdateTasks("remove", null, null, null, null, updatedTasks);
   };
 
   clearCompletedTasks = () => {
-    // remove all tasks that have been marked as completed
-    const updatedTasks = this.state.tasks.filter(
-      (task) => !task.startsWith("✅")
-    );
-    this.setState({ tasks: updatedTasks });
+    const updatedTasks = clearCompletedTasks(this.props.tasks);
+    this.props.onUpdateTasks("clear", null, null, null, null, updatedTasks);
   };
 
   render() {
-    const hasCompletedTasks = this.state.tasks.some((task) =>
-      task.startsWith("✅")
-    );
+    const hasCompletedTasks = this.props.tasks.some((task) => task.completed);
+
     return (
       <div className="rounded-lg p-8 shadow-md space-y-4">
         <h2 className="text-2xl font-semibold mb-4 text-center">Task List</h2>
         <div className="task-list__input mb-2 flex items-center">
           <input
             type="text"
-            placeholder="Enter a task"
-            onChange={(e) => this.setState({ newTask: e.target.value })}
-            value={this.state.newTask}
-            className="rounded-md py-2 px-2 w-40 text-black"
+            placeholder="Enter a task ..."
+            onChange={(e) => this.setState({ taskInput: e.target.value })}
+            value={this.state.taskInput}
+            className="rounded-md py-2 px-2 w-40 mr-2 text-white"
+            style={{ backgroundColor: "transparent" }}
           />
+          <div className="flex items-center">
+            <label className="mr-2 text-gray-600">Pomodoros:</label>
+            <input
+              type="number"
+              placeholder="Pomodoros"
+              onChange={(e) =>
+                this.setState({ pomodorosInput: e.target.value })
+              }
+              value={this.state.pomodorosInput}
+              className="rounded-md py-2 px-4 w-16 text-white"
+              style={{ backgroundColor: "transparent" }}
+            />
+          </div>
+
           <button
             onClick={this.addTask}
             className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-2 rounded-md ml-2"
@@ -69,15 +82,19 @@ class TaskList extends Component {
           </button>
         </div>
         <ul className="task-list__list">
-          {this.state.tasks.map((task, index) => (
+          {this.props.tasks.map((task, index) => (
             <li key={index} className="mb-0 flex items-center">
               <input
                 type="checkbox"
-                onChange={() => this.markTaskCompleted(index)}
-                checked={task.startsWith("✅")}
+                onChange={() => this.toggleTaskCompleted(index)}
+                checked={task.completed}
                 className="mr-2"
               />
-              <span className="text-lg">{task.replace("✅", "")}</span>
+              <span
+                className={task.completed ? "line-through text-lg" : "text-lg"}
+              >
+                {task.text}
+              </span>
               <button
                 className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-2 rounded-md ml-auto"
                 onClick={() => this.removeTask(index)}
@@ -89,8 +106,8 @@ class TaskList extends Component {
         </ul>
         {hasCompletedTasks && (
           <button
-            onClick={hasCompletedTasks && this.clearCompletedTasks}
-            className="text-red-800 hover:bg-red-600 hover:text-white te bg-blue-500 font-semibold py-4 px-4 rounded-md mx-auto block mt-4"
+            onClick={this.clearCompletedTasks}
+            className="text-white hover:bg-red-800 hover:text-white te bg-blue-500 font-semibold py-2 px-2 rounded-md mx-auto block mt-4"
           >
             Clear Completed Tasks
           </button>
